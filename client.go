7890c31b3335a2f0c08938f21f1192e75ecf246e
brucewayne8 <santhatipurush8@gmail.com>
2020-09-18 19:58:14 +0530

    after push function

--- client.go
+++ client.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func get(url string) {
@@ -23,7 +26,40 @@ func get(url string) {
 	fmt.Println(string(body))
 }
 
-func post(url string, data string) {}
+func post(url string, data string) {
+	parts := strings.Split(data, "&&")
+	leng := len(parts)
+
+	if leng == 0 {
+		panic("Provide the field values")
+	}
+
+	var mp = make(map[string]string)
+
+	for i := 0; i < leng; i++ {
+		fieldValue := strings.Split(parts[i], "=")
+		if len(fieldValue) != 2 {
+			panic("Provide the field values in correct format")
+		}
+		mp[fieldValue[0]] = fieldValue[1]
+	}
+
+	jsonbody, err := json.Marshal(mp)
+
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(jsonbody))
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
 
 func main() {
 	method, url := os.Args[1], os.Args[2]
